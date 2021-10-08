fd7d3a98eb3c503eef870a322478289e11b67a4d
Angelo-scut <499981793@qq.com>
2021-10-08 22:00:00 +0800

    jsontodata
    完善了livewire到label

--- labelme/jsontodataset.py
+++ labelme/jsontodataset.py
@@ -12,22 +12,25 @@ import yaml
 import cv2
 from labelme import utils
 
+
 def ellipse_type(json_file, list, out, img_size):
     for i in range(0, len(list)):
         path = os.path.join(json_file, list[i])  # 获取每个json文件的绝对路径
-        filename = list[i][:-5]       # 提取出.json前的字符作为文件名，以便后续保存Label图片的时候使用
+        filename = list[i][:-5]  # 提取出.json前的字符作为文件名，以便后续保存Label图片的时候使用
         extension = list[i][-4:]
         if extension == 'json':
             if os.path.isfile(path):
                 data = json.load(open(path))
                 img = utils.image.img_b64_to_arr(data['imageData'])  # 根据'imageData'字段的字符可以得到原图像
                 # lbl为label图片（标注的地方用类别名对应的数字来标，其他为0）lbl_names为label名和数字的对应关系字典
-                lbl, lbl_names = utils.shape.labelme_shapes_to_label(img.shape, data['shapes'])   # data['shapes']是json文件中记录着标注的位置及label等信息的字段
+                lbl, lbl_names = utils.shape.labelme_shapes_to_label(img.shape, data[
+                    'shapes'])  # data['shapes']是json文件中记录着标注的位置及label等信息的字段
 
                 ebbx = utils.shape.bbox_for_eliipse(img.shape, data['shapes'])
                 # captions = ['%d: %s' % (l, name) for l, name in enumerate(lbl_names)]
-                lbl_viz = utils.shape.ellipse_with_angle(img, ebbx[1]*img.shape[1], ebbx[2]*img.shape[0],
-                                                         ebbx[3]*img.shape[1], ebbx[4]*img.shape[0], ebbx[5], 255)  # cx/img_shape[1], cy/img_shape[0], a/img_shape[1], b/img_shape[0]
+                lbl_viz = utils.shape.ellipse_with_angle(img, ebbx[1] * img.shape[1], ebbx[2] * img.shape[0],
+                                                         ebbx[3] * img.shape[1], ebbx[4] * img.shape[0], ebbx[5],
+                                                         255)  # cx/img_shape[1], cy/img_shape[0], a/img_shape[1], b/img_shape[0]
                 # lbl_viz = utils.draw.draw_label(lbl, img, captions)
                 out_dir = out + osp.basename(list[i])[:-5]
                 out_dir = osp.join(osp.dirname(list[i]), out_dir)
@@ -61,10 +64,11 @@ def ellipse_type(json_file, list, out, img_size):
                         for e in range(len(ebbx)):
                             if e == 0:
                                 f.write(str(ebbx[e]) + ' ')
-                            elif e == len(ebbx)-1:
+                            elif e == len(ebbx) - 1:
                                 f.write(str(format(ebbx[e], '.4f')))
                             else:
                                 f.write(str(format(ebbx[e], '.4f')) + ' ')
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -72,10 +76,10 @@ def main():
     # parser.add_argument('-o', '--out', default=None)
     # args = parser.parse_args()
     img_size = 640
-    json_file = "./testimage"#args.json_file
+    json_file = "./testimage"  # args.json_file
     out = "./testimage/"
     color_mask = np.array([255, 0, 0], dtype=np.uint8)  # 可是化的颜色
-    list = os.listdir(json_file)   # 获取json文件列表
+    list = os.listdir(json_file)  # 获取json文件列表
     # ellipse_type(json_file, list, out, img_size)
     for i in range(0, len(list)):
         path = os.path.join(json_file, list[i])  # 获取每个json文件的绝对路径
@@ -86,12 +90,14 @@ def main():
                 data = json.load(open(path))
                 img = utils.image.img_b64_to_arr(data['imageData'])  # 根据'imageData'字段的字符可以得到原图像
                 # lbl为label图片（标注的地方用类别名对应的数字来标，其他为0）lbl_names为label名和数字的对应关系字典
-                lbl, lbl_names = utils.shape.labelme_shapes_to_label(img.shape, data['shapes'], isClose=False)  # data['shapes']是json文件中记录着标注的位置及label等信息的字段
+                lbl, lbl_names = utils.shape.labelme_shapes_to_label(img.shape, data['shapes'],
+                                                                     isClose=False)  # data['shapes']是json文件中记录着标注的位置及label等信息的字段
                 out_dir = out + osp.basename(list[i])[:-5]
                 out_dir = osp.join(osp.dirname(list[i]), out_dir)
                 if not osp.exists(out_dir):
                     os.mkdir(out_dir)
                 PIL.Image.fromarray(lbl).save(osp.join(out_dir, '{}_mask.png'.format(filename)))
+
 
 def checkmsak():
     mask = Image.open('./testimage/back10166/back10166_mask.png').convert('L')
@@ -110,7 +116,51 @@ def checkmsak():
     mask.show()
     # im.show()
 
+
+def lwlabel(file_path, out_path):
+    file_list = os.listdir(file_path)
+    if not osp.exists(out_path):
+        os.mkdir(out_path)
+    out_visual = os.path.join(out_path, "visual")
+    if not osp.exists(out_visual):
+        os.mkdir(out_visual)
+    color_list = [[0, 0, 255],
+                  [0, 255, 0],
+                  [255, 0, 0]]
+    for i in range(0, len(file_list)):
+        path = os.path.join(file_path, file_list[i])  # 获取每个json文件的绝对路径
+        filename = file_list[i][:-5]  # 提取出.json前的字符作为文件名，以便后续保存Label图片的时候使用
+        extension = file_list[i][-4:]
+        if extension == 'json':
+            if os.path.isfile(path):
+                data = json.load(open(path))
+                img = utils.image.img_b64_to_arr(data['imageData'])  # 根据'imageData'字段的字符可以得到原图像
+                lbl, lbl_names = utils.shape.labelme_shapes_to_label(img.shape, data['shapes'], isClose=True)
+                cv2.imwrite(osp.join(out_path, '{}_mask.png'.format(filename)), lbl.astype(np.uint8))
+                cv2.imwrite(osp.join(out_path, '{}.png'.format(filename)), img.astype(np.uint8))
+                # PIL.Image.fromarray(lbl).save(osp.join(out_path, '{}_mask.png'.format(filename)))
+                # PIL.Image.fromarray(img).save(osp.join(out_path, '{}.png'.format(filename)))
+                img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+                for j in range(1, lbl.max()+1):
+                    temp = lbl.copy()
+                    temp[temp != j] = 0
+                    temp[temp == j] = 1
+                    temp = temp.astype(np.bool8)
+                    color_mask = np.array(color_list[j-1], dtype=np.uint8)
+                    img[temp] = img[temp] * 0.8 + color_mask * 0.2
+                cv2.imwrite(osp.join(out_visual, '{}_visual.png'.format(filename)), img.astype(np.uint8))
+                # PIL.Image.fromarray(img).save(osp.join(out_visual, '{}_visual.png'.format(filename)))
+
+
 if __name__ == '__main__':
-    main()
+    # main()
     # print('Finished!')
-    checkmsak()
+    # checkmsak()
+    file_path = "E:\OneDrive\My_paper\Program\Gapcontrol\data\\0713\\train\weldpool\\train"
+    save_path = "E:\OneDrive\My_paper\Program\Gapcontrol\data\\0713\\train\weldpool\\label"
+    lwlabel(file_path, save_path)
+    # img = cv2.imread("E:\OneDrive\My_paper\Program\Gapcontrol\data\\0713\\train\weldpool\\test\\test\\35_mask.png", 0)
+    # img = img * 100
+    # print(np.max(img))
+    # cv2.imshow("s", img)
+    # cv2.waitKey()
